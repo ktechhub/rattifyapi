b69f8ca82f63542729d482471b913f5a2896e22e
Mumuni Mohammed <mumunim10@gmail.com>
2023-09-05 18:06:32 +0100

    feat: updated rattifyapi

--- rattifyapi/__init__.py
+++ rattifyapi/__init__.py
@@ -3,4 +3,4 @@ import os
 """Script used to define constants used across codebase."""
 
 SECRET_KEY = os.getenv("SECRET_KEY", None)
-API_URL = "https://users.staging.rattify.com/api/v1/users/"
+API_URL = "https://users.rattify.com/api/v1/users/"
